4218697231c0bc1d5f9e1c0572272a5887a5e81c
i2gan <i2gun0f@gmail.com>
2023-02-18 16:30:47 +0400

    Lesson 33. Введение в параметризацию. (Generics)

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by i2gan on 17.02.2023
  */
 
 public class Test {
     public static void main(String[] args) {
+        //////////////// до Java 5 ///////////////
+        List animals = new ArrayList();
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("frog");
 
-        // Upcasting - восходящее преобразование
-        // Animal animal = new Dog();
+        // делаем downcast
+        String animal = (String) animals.get(1);
+        System.out.println(animal);
 
-        Dog dog = new Dog();
-        // Upcasting - восходящее преобразование
-        Animal animal = dog;
+        // с появлением дженериков (jenerics)//
+        List<String> animals2 = new ArrayList<String>();
+        animals2.add("cat");
+        animals2.add("dog");
+        animals2.add("frog");
 
-        // Douncasting - нисходящее преобразование
-        Dog dog2 = (Dog) animal;
-        dog2.eat();
-        dog2.bark();
-        /** вывод
-         *
-         * Animal is eating..
-         * Dog is barking..
-         */
+        String animal2 = animals2.get(1);
 
-        Animal a = new Animal();
-        Dog d = (Dog) a;
-        d.bark(); // ошибка, так делать нельзя
+        /////////// Java 7 /////////////////////////
+        List<String> animal3 = new ArrayList<>();
+
     }
 }
