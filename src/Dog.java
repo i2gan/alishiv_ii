ea8d9cd90c9461f9e708db1859c591201a17086d
i2gan <i2gun0f@gmail.com>
2023-02-21 10:12:21 +0400

    Lesson 35. Анонимные классы

--- src/Dog.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by i2gan on 21.02.2023
- */
-
-public class Dog extends Animal{
-}
--- src/Test.java
+++ src/Test.java
@@ -1,34 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by i2gan on 17.02.2023
  */
 
+interface AbleToEat {
+    public void eat();
+}
 public class Test {
     public static void main(String[] args) {
-        List<Animal> listOfAnimals = new ArrayList<>();
-        listOfAnimals.add(new Animal(1));
-        listOfAnimals.add(new Animal(2));
+        // анонимный класс, объект наследник, переопределение метода
+        AbleToEat ableToEat = new AbleToEat() {
+            @Override
+            public void eat() {
+                System.out.println("Someone is eating..");
+            }
+        };
 
-        List<Dog> listOfDogs = new ArrayList<>();
-        listOfDogs.add(new Dog());
-        listOfDogs.add(new Dog());
-
-        test(listOfAnimals);
-        test(listOfDogs);
-    }
-
-    /*
-     * Object
-     *     Animal
-     *         Dog
-     *
-     * <? extends Animal> подразумевает Animal и наследники (здесь Dog)
-     * <? super Animal> подразумеваем Animal и родители (здесь Object)
-     */
-    private static void test(List<? extends Animal> list) {
-        for (Animal animal: list)
-            animal.eat();
+        ableToEat.eat();
     }
 }
